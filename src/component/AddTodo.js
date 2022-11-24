6860d7a89f86b5b70e33bbfad2d9d1db41bcc80f
pranab56 <pronabhalder53@gmail.com>
2022-11-24 15:59:36 +0600

    delete items

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -27,14 +27,14 @@ const AddTodo = () => {
         
             <div>
                 <label class="text-gray-700 dark:text-gray-200" for="title">Title</label>
-                <input id="title" name='title' type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                <input id="title" name='title' type="text" required class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
             </div>
 
           
 
             <div className='mt-3'>
                 <label class="text-gray-700 dark:text-gray-200" for="desc">Description</label>
-                <input id="desc" name='desc' type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                <input id="desc" name='desc' type="text" required class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
             </div>
 
 
